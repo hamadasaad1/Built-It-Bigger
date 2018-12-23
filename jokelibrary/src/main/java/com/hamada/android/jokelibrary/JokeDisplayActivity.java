70715804315e3100a273ebf9b62faf7a4bbb99ef
hamadasaad1 <hamadasaad252@gmail.com>
2018-12-23 15:02:42 +0200

    build it bigger app for gradle

--- jokelibrary/src/main/java/com/hamada/android/jokelibrary/JokeDisplayActivity.java
+++ jokelibrary/src/main/java/com/hamada/android/jokelibrary/JokeDisplayActivity.java
@@ -1,5 +1,6 @@
 package com.hamada.android.jokelibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,7 +12,8 @@ public class JokeDisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_display);
 
-        if (getIntent() != null) {
+        Intent intent=getIntent();
+        if (intent != null&&intent.hasExtra("jokes")) {
             String joke = getIntent().getStringExtra("jokes");
             TextView jokeText = findViewById(R.id.joke_display_textView);
             jokeText.setText(joke);
